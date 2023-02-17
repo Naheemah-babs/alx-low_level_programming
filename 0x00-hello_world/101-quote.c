de9e07c843263c3c57b902d69a62c4bffe9bc1c2
Naheemah-babs <babatundenaheemot@gmail.com>
2023-02-16 21:10:27 -0800

    101 unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <stdio.h>
+#include <unistd.h>
 /**
- * mai - prints a line to stderr
+ * main - prints a line to stderr
  * Return: 1 (SUCCESS)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
